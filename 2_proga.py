f97cef8b8d6993dd6a2287c8c16b2c967b712c7f
Dmitry Mikhailov <arlit112@gmail.com>
2022-09-22 21:46:31 +0300

    3 rogram

--- 2_proga.py
+++ 2_proga.py
@@ -20,14 +20,16 @@
 # print(num)
     
 # 2 вариант
-from    random  import randint
-count=5
-nums = [randint(1, 20) for i in range(count)]
-print (nums)
-mymax = nums[0]
-mymin = nums[0]
-for i in range(count):
-    if nums[i] > mymax : myMax=nums[i]
-    if nums[i] < mymin : myMin=nums[i]
-print ('Max:', mymax, 'Min:', mymin)
+# from    random  import randint
+# count=5
+# nums = [randint(1, 20) for i in range(count)]
+# print (nums)
+# mymax = nums[0]
+# mymin = nums[0]
+# for i in range(count):
+#     if nums[i] > mymax : myMax=nums[i]
+#     if nums[i] < mymin : myMin=nums[i]
+# print ('Max:', mymax, 'Min:', mymin)
+
+
 
